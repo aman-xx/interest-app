9fc8e8d337e14db68791ac04b244b5d2dee89f04
Aman Kumar <amankumarmznup@gmail.com>
2022-05-01 21:59:40 +0530

    completed project

--- backend/schemas/postedBy.js
+++ backend/schemas/postedBy.js
@@ -1,7 +1,6 @@
-export default{
-    name:'postedBy',
-    title:'PostedBy',
-    type:'reference',
-    to:[{type:'user'}]
-   
-}
+export default {
+    name: 'postedBy',
+    title: 'PostedBy',
+    type: 'reference',
+    to: [{ type: 'user' }],
+  };
